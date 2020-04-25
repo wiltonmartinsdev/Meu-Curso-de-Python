c5b3efb4909d873c185ffb288e4100c78ce08998
wiltonmartinsdev <wiltonliramartins@protonmail.com>
2020-04-25 15:10:06 -0300

    Erro Corrigido no pygame

--- Arquivos.py/Desafio 021_Tocando um mp3.py
+++ Arquivos.py/Desafio 021_Tocando um mp3.py
@@ -1,5 +1,8 @@
 # Faça um programa em Python que abra e reproduza o aúdio de um arquivo mp3.
-import os; print(os.getcwd())
 
+from pygame import mixer
+mixer.init()
+mixer.music.load('capoeira.mp3')
+input()
 
 
