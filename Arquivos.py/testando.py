ae4840694de3d19075e26f456672e11515dc3b24
wiltonmartinsdev <wiltonliramartins@protonmail.com>
2020-04-30 11:05:29 -0300

    modificando o exercicio 21

--- Arquivos.py/testando.py
+++ Arquivos.py/testando.py
@@ -1,27 +1,3 @@
-num = int(input('Digite um número de termo para sequência Fibonacci: '))
+import playsound
 
-cont = 1
-
-
-
-anterior = 0
-
-proxima = 1
-
-soma = 1
-
-
-
-while cont <= num:
-
-     print(anterior, end='-')
-
-     cont += 1
-
-     soma = proxima + anterior
-
-     anterior = proxima
-
-     proxima = soma
-
-print('Fim')
+playsound.playsound('ex021.mp3')
