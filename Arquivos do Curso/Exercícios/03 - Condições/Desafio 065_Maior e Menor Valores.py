263899a91a259929edfec7aaa5a9e35eb9ea2c07
Wilton Lira Martins <wiltonliramartins@protonmail.com>
2020-06-01 17:03:30 -0300

    atualização das aulas

--- Arquivos do Curso/Exercícios/03 - Condições/Desafio 065_Maior e Menor Valores.py
+++ Arquivos do Curso/Exercícios/03 - Condições/Desafio 065_Maior e Menor Valores.py
@@ -2,6 +2,8 @@
 Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a média entre todos os valores e qual foi o maior e menor
 valor lido. O programa deve perguntar ao usuário se ele quer ou não digitar valores.
 '''
+
+print('{:=^50}'.format(' MAIOR E MENOR VALOR DIGITADO '))
 resp = 's'
 quant_num = soma = maior = menor = 0
 
@@ -19,4 +21,5 @@ while resp == 's':
     soma += numero
 media = soma / quant_num
 
-print(f'Foram digitados {quant_num} números, sendo {maior} o maior e {menor} o menor valor digitado, sendo {soma} a soma total entre eles e tendo como média o valor de {media:.2f}.')
+print(f'\nForam digitados {quant_num} números, sendo {maior} o maior e {menor} o menor, valor digitado, sendo {soma} a soma total entre eles e tendo como média o valor de {media:.2f}.')
+print('=' * 141)
