e6fada958c9fb72b7129c19e08348cceebd1f4b2
wiltonmartinsdev <wiltonliramartins@protonmail.com>
2020-09-22 16:17:18 -0300

    Atualização do curso

--- Arquivos do Curso/Exercícios/04 - Tuplas, Listas, Dicionários e Funções/4.2 - Listas/Desafio 089_Boletim com listas compostas.py
+++ Arquivos do Curso/Exercícios/04 - Tuplas, Listas, Dicionários e Funções/4.2 - Listas/Desafio 089_Boletim com listas compostas.py
@@ -2,27 +2,29 @@
  Crie um programa que leia nome e duas notas de vários alunos e guarde tudo em uma lista composta. No final, 
  mostre um boletim contendo a média de cada um e permita que o usuário possa mostrar as notas de cada aluno individualmente.
  '''
-lista_completa = list()
-lista_cadastro = list()
+boletim = list()
 media = 0
 
 while True:
-    lista_cadastro.append(str(input('Digite seu nome: ')))
+    nome = str(input('Qual o seu nome? ')).upper()
     n1 = float(input('Digite a Nota 1: '))
-    lista_cadastro.append(n1)
     n2 = float(input('Digite a Nota 2: '))
-    lista_cadastro.append(n2)
     media = (n1 + n2) / 2
-    lista_cadastro.append(media)
-    lista_completa.append(lista_cadastro[:])
-    lista_cadastro.clear()
+    boletim.append([nome, [n1,n2], media])
     resp = str(input('Você deseja continuar [S/N]? ')).upper()
     while resp not in 'SN':
         resp = str(input('Valor Inválido! Você deseja continuar [S/N]? ')).upper()
     if resp == 'N':
         break
-print(lista_completa)
-print(lista_completa[0][3])
-print(f'Nota 1: {lista_completa[0][1]}')
-print(f'Nota 2: {lista_completa[0][2]}')
-
+print(f'{"Nº":<4}{"NOME":<10}{"MÉDIA":>8}')
+for i, a in enumerate(boletim):
+    print(f'{i:<4}{a[0]:<10}{a[2]:>8.1f}')
+while True:
+    opcao = int(input('\nDeseja pesquisar nota de qual aluno? (999 para interromper a pesquisa): '))
+    if opcao <= len(boletim)-1:
+        print(f'As notas de {boletim[opcao][0]} são: {boletim[opcao][1]}')
+    elif opcao > len(boletim)-1 and opcao < 999:
+        print('Aluno não Cadastrado!')
+    elif opcao == 999:
+        print('Pesquisa Finalizada!')
+        break
